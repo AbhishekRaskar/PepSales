cc0bd29cf92410b2e5be5d3872cfc89f6c0a546f
ASUS <abhiraskar@gmail.com>
2024-05-31 14:26:23 +0530

    Frontend Code with Styling Part of the Card

--- pepsales/src/Pages/HomePage.jsx
+++ pepsales/src/Pages/HomePage.jsx
@@ -7,20 +7,40 @@ import {
   SimpleGrid,
   Button,
   Spinner,
+  Image,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCompanyData } from "../Redux/CompanyReducer/acton";
+import Logo1 from "../Images/Logo1.png";
+import Logo2 from "../Images/Logo2.png";
+import Logo3 from "../Images/Logo3.png";
+import Logo4 from "../Images/Logo4.png";
+import Logo5 from "../Images/Logo5.png";
+import Logo6 from "../Images/Logo6.png";
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const stocks = useSelector((state) => state.stocks);
   const [showDetails, setShowDetails] = useState({});
 
+  const cardBg = useColorModeValue("white", "gray.800");
+  const cardTextColor = useColorModeValue("gray.800", "white");
+  const containerBg = useColorModeValue("gray.100", "gray.900");
+  const detailsBg = useColorModeValue("gray.100", "gray.700");
+
+  const logoMap = {
+    Logo1,
+    Logo2,
+    Logo3,
+    Logo4,
+    Logo5,
+    Logo6,
+  };
+
   useEffect(() => {
     dispatch(fetchCompanyData());
   }, [dispatch]);
-
-  console.log(stocks, "stocks");
 
   if (!stocks || stocks.length === 0) {
     return (
@@ -38,28 +58,38 @@ const HomePage = () => {
   };
 
   return (
-    <Flex justify="center" align="center" p={4}>
-      <SimpleGrid columns={[1, 2, 3]} spacing={4} w="full">
+    <Flex justify="center" align="center" p={4} bg={containerBg}>
+      <SimpleGrid columns={[1, 2, 3]} spacing={6} w="full">
         {stocks.map((stock, index) => (
           <Box
             key={`${stock.symbol1}-${stock.symbol2}-${index}`}
-            p={4}
+            p={6}
             borderWidth="1px"
             borderRadius="lg"
             overflow="hidden"
             boxShadow="lg"
+            bg={cardBg}
+            color={cardTextColor}
           >
-            <Text fontSize="xl" fontWeight="bold">
+            <Center>
+              <Image
+                src={logoMap[`Logo${index % 6 + 1}`]}
+                boxSize="100px"
+                mb={4}
+                alt={`Logo${index % 6 + 1}`}
+              />
+            </Center>
+            <Text fontSize="2xl" fontWeight="bold" textAlign="center">
               {stock.symbol1} / {stock.symbol2}
             </Text>
-            <Text fontSize="md" color="gray.500">
+            <Text fontSize="lg" color="gray.500" mt={2}>
               Min Price: {stock.minPrice || "N/A"}
             </Text>
-            <Text fontSize="md" color="gray.500">
+            <Text fontSize="lg" color="gray.500">
               Max Price: {stock.maxPrice || "N/A"}
             </Text>
-            <Center mt={2}>
-              <Text fontSize="2xl">
+            <Center mt={4}>
+              <Text fontSize="2xl" fontWeight="bold">
                 Min Lot Size: {stock.minLotSize || "N/A"}
               </Text>
             </Center>
@@ -73,7 +103,7 @@ const HomePage = () => {
                 : "More Details"}
             </Button>
             {showDetails[`${stock.symbol1}-${stock.symbol2}`] && (
-              <Box mt={4} p={2} borderWidth="1px" borderRadius="md">
+              <Box mt={4} p={4} borderWidth="1px" borderRadius="md" bg={detailsBg}>
                 <Text fontSize="md">
                   Price Precision: {stock.pricePrecision || "N/A"}
                 </Text>
